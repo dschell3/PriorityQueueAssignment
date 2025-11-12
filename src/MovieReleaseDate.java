9b61bfe85c892f2a90a1e997e149c5e000480098
dschell3 <dschell9876@gmail.com>
2025-11-12 11:18:36 -0800

    #3 done, MovieReleaseDate class implemented

--- src/MovieReleaseDate.java
+++ src/MovieReleaseDate.java
@@ -1,2 +1,17 @@
-public class MovieReleaseDate {
+/**
+ * MovieReleaseDate comparator class
+ * Compares movies based on their release date
+ * Earlier dates have higher priority
+ */
+
+import java.util.Comparator;
+
+ public class MovieReleaseDate implements Comparator<Movie> {
+     @Override
+     public int compare(Movie a, Movie b) {
+        return a.getRelease_date().compareTo(b.getRelease_date());
+         // if < 0 ; a was released before b
+         // if > 0 ; b was released before a
+         // if = 0 ; a was released at the same time as b
+     }
 }
